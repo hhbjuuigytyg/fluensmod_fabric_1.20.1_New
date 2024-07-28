ba105ce0a3bea215cad541c0288cbe7d3f3e3bba
hhbjuuigytyg <asdfinder1993@gmail.com>
2024-07-28 21:30:48 +0300

    Initial commit

---
+++ src/main/java/konotopartem/modid/FluensModClient.java
@@ -0,0 +1,11 @@
+package konotopartem.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FluensModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/konotopartem/modid/FluensModDataGenerator.java
@@ -0,0 +1,11 @@
+package konotopartem.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FluensModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
